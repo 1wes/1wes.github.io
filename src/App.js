3106e00d67f1f4bfb911d045ead83352fb022fa6
1wes <okemwawes@gmail.com>
2022-02-14 15:30:44 +0300

    determine the winner or tie

--- src/App.js
+++ src/App.js
@@ -110,3 +110,32 @@ class Game extends React.Component{
     }
 }
 export default Game;
+
+function calculateTheWinner(squares){
+
+    const winnerCombinations=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+
+    // compare whether any of the combinations has been achieved
+    for(let i=0; i<=winnerCombinations.length; i++){
+
+        const [a, b, c]=winnerCombinations[i];
+
+        if(squares[a]&&squares[a]===squares[b]&&squares[a]===squares[c]){
+
+            return squares[a];
+        }
+    }
+
+    // incase of a tie, 
+    return null;
+}
