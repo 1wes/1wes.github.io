0390f7bc76de815527af8075bcda95f160a6543a
1wes <okemwawes@gmail.com>
2022-03-20 18:50:12 +0300

    Updates

---
+++ precache-manifest.3900373270728015c7f7855ced6a158d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ec951cf0ae404ef7f62",
+    "url": "/static/css/main.e0f8d059.chunk.css"
+  },
+  {
+    "revision": "3ec951cf0ae404ef7f62",
+    "url": "/static/js/main.3ec951cf.chunk.js"
+  },
+  {
+    "revision": "fa3599f443690d67d8b8",
+    "url": "/static/js/1.fa3599f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1525b97b4cabd824852f228f0a8022c6",
+    "url": "/index.html"
+  }
+];
