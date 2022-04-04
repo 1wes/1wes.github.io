49c448c7029290ece390d1f8eae692958ca1dfce
1wes <okemwawes@gmail.com>
2022-04-04 18:26:59 +0300

    Updates

---
+++ precache-manifest.83afdc2deae7069476a65424b60a87a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0b21e586eb2cd5abb37",
+    "url": "/static/css/main.0c7a3da9.chunk.css"
+  },
+  {
+    "revision": "b0b21e586eb2cd5abb37",
+    "url": "/static/js/main.b0b21e58.chunk.js"
+  },
+  {
+    "revision": "9182b78f3a5d646f80af",
+    "url": "/static/js/1.9182b78f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ae651d35c860999d3bc9055726ea686",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b039937fd4b7e3286442658f2c709103.js"
+  "/precache-manifest.83afdc2deae7069476a65424b60a87a0.js"
 );
 
 workbox.clientsClaim();
