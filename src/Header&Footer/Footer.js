66eba5374647fd93dcce92536fdf99576ac19880
1wes <okemwawes@gmail.com>
2022-04-13 20:06:52 +0300

    minor change, spacing

--- src/Header&Footer/Footer.js
+++ src/Header&Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCopyright} from '@fortawesome/free-solid-svg-icons'
+
 let currentYear=new Date().getFullYear();
 
 class Footer extends React.Component{
