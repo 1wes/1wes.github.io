d9d7f9e09711729e55538876f2ddbb1c2d0e36da
1wes <okemwawes@gmail.com>
2022-05-01 17:10:43 +0300

    add how to play link to the footer

--- src/components/Header&Footer/Footer.js
+++ src/components/Header&Footer/Footer.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCopyright} from '@fortawesome/free-solid-svg-icons'
+import {Link} from 'react-router-dom';
+
+function Instructions(){
+
+    return(
+        <div className='how-to-play'>
+            <div className="instructions-link">
+                <Link to="./components/Instructions/how-to-play" style={{textDecoration:"none"}}>
+                    How to Play?
+                </Link>
+            </div>
+        </div>
+    )
+}
 
 let currentYear=new Date().getFullYear();
 
@@ -11,6 +25,9 @@ class Footer extends React.Component{
         return(
             <footer className='footer'>
                 <div className='footer_details'>
+                    <div>
+                        <Instructions />
+                    </div>
                     <span>
                         <span className='copyright-icon'><FontAwesomeIcon icon={faCopyright} /></span>
                         Wes <span className='copyright_year'>{currentYear}</span>
