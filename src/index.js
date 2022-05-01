a8c71df6bf2a257dd757aef1f4028e535fab67de
1wes <okemwawes@gmail.com>
2022-05-01 15:25:07 +0300

    wrap Game component in BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 import Game from './App';
+import {BrowserRouter} from 'react-router-dom'
+
+const root=document.getElementById("root");
 
 ReactDom.render(
-  <Game />,
-  document.getElementById("root")
+  // all rendering goes inside BrowserRouter 
+  <BrowserRouter>
+    <Game />
+  </BrowserRouter>, root
 );
