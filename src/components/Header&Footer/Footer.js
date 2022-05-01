a0082768242d2392be1e9c25db23e1b7337b0840
1wes <okemwawes@gmail.com>
2022-05-01 15:51:40 +0300

    move Header&Footer folder into components folder

---
+++ src/components/Header&Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCopyright} from '@fortawesome/free-solid-svg-icons'
+
+let currentYear=new Date().getFullYear();
+
+class Footer extends React.Component{
+
+    render(){
+
+        return(
+            <footer className='footer'>
+                <div className='footer_details'>
+                    <span>
+                        <span className='copyright-icon'><FontAwesomeIcon icon={faCopyright} /></span>
+                        Wes <span className='copyright_year'>{currentYear}</span>
+                    </span>
+                </div>
+            </footer>
+        )
+    }
+}
+export default Footer;
