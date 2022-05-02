fe377313e3fe0017239734584d42383b78b5d4f3
1wes <okemwawes@gmail.com>
2022-05-02 12:17:42 +0300

    add outlet in Footer component for better matching and layout persistence

--- src/components/Header&Footer/Footer.js
+++ src/components/Header&Footer/Footer.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCopyright} from '@fortawesome/free-solid-svg-icons'
-import {Link} from 'react-router-dom';
+import {Link, Outlet} from 'react-router-dom';
 
 function Instructions(){
 
     return(
         <div className='how-to-play'>
             <div className="instructions-link">
-                <Link to="./components/Instructions/how-to-play" style={{textDecoration:"none"}}>
+                <Link to="/instructions" style={{textDecoration:"none"}}>
                     How to Play?
                 </Link>
             </div>
+            <Outlet />
         </div>
     )
 }
