ac9ae7c3edbb5e802ceeeca4c33658d75521de84
1wes <okemwawes@gmail.com>
2022-05-02 12:22:04 +0300

    route the link to instructions page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 import Game from './App';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Howtoplay from './components/Instructions/how-to-play';
 
 const root=document.getElementById("root");
 
 ReactDom.render(
   // all rendering goes inside BrowserRouter 
   <BrowserRouter>
-    <Game />
+    <Routes>
+      <Route path='/' element={<Game />} />
+        <Route path='instructions' element={<Howtoplay />} />
+       <Route />
+    </Routes>
   </BrowserRouter>, root
 );
