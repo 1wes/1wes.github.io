70484d84548e8bf0f958299fcbb82fe039f4a505
1wes <okemwawes@gmail.com>
2022-05-06 15:51:55 +0300

    show how the game is played

--- src/components/Instructions/how-to-play.js
+++ src/components/Instructions/how-to-play.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import Layout from '../Header&Footer/Layout'
+import './how-to-play.css'
 
 function Instructions(){
 
     return(
         <main style={{color:"whitesmoke"}}>
             <h3>
-                Instructions coming soon....
+                How to play Tic-Tac-Toe
             </h3>
+
+            {/* container for instructions */}
+            <div>
+
+                {/* container with the layout */}
+                <div className='main-content'>
+                    <ul className='instructions-list'>
+                        <li className='instruction'>
+                            <div className='instruction-number'>
+                                <span>1</span> 
+                            </div>
+
+                            <p> The game is played on a grid that's 3 squares by 3 squares.</p>
+                        </li>
+                        <li className='instruction'>
+                            <div className='instruction-number'>
+                                <span>2</span> 
+                            </div>
+
+                            <p>One player is <span id='x'>X</span>, and the other is <span id='o'>O</span>. Players take turns putting their marks in empty squares</p>
+                        </li>
+                        <li className='instruction'>
+                            <div className='instruction-number'>
+                                <span>3</span>
+                            </div>
+
+                            <p>The first player to get 3 of their marks in a row (up, down, across, or diagonally) is the winner</p>
+
+                        </li>
+
+                        <li className='instruction'>
+                            <div className='instruction-number'>
+                                <span>4</span>
+                            </div>
+
+                            <p>When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.</p>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
         </main>
     )
 }
