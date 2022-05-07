28c67655bdb25421846ad3f3e0bf75476fe79d2c
1wes <okemwawes@gmail.com>
2022-05-07 17:55:07 +0300

    make the navbar sticky --add sticky class

--- src/components/Header&Footer/Header.js
+++ src/components/Header&Footer/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './Header.css'
 
+
+// var navbar=document.getElementById("nav-bar");
+
+// window.onscroll=function (){
+//     stickNavbar()
+// };
+
+// var navbar=document.getElementById("nav-bar");
+
+// var navbarPosition=navbar.offsetTop;
+
+// function stickNavbar(){
+//     if(window.scrollY>navbarPosition){
+//         navbar.classList.add("sticky");
+//     }else{
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
 class Header extends React.Component{
 
     render(){
 
         return(
-            <nav>
+            <nav id='nav-bar'>
                 <ul>
                     <li style={{color:"whitesmoke"}}>
                         <a href='javascript:void(0);'>tic-tac-toe</a>
