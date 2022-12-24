2e502f8d7fca3700c9e94fda49e6f1556c78e9b5
1wes <okemwawes@gmail.com>
2022-12-24 12:16:53 +0300

    avoid passing the root container into the renderer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 import React from 'react';
 import Game from './App';
 import {HashRouter, Routes, Route} from 'react-router-dom'
 import Howtoplay from './components/Instructions/how-to-play';
 
-const root=document.getElementById("root");
+const root=ReactDom.createRoot(document.getElementById("root"));
 
-ReactDom.render(
+root.render(
   // all rendering goes inside HashRouter
   // enables storing the URL in the hash portion since it cannot be sent to the server in github Pages
 
-  <HashRouter hashType='slash' basename='/' >
+  <HashRouter hashType='slash'>
     <Routes>
       <Route path='/' element={<Game/>} />
       <Route path='instructions' element={<Howtoplay/>} />
     </Routes>
-  </HashRouter>, root
+  </HashRouter>
 );
