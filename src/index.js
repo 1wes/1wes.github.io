982575d3c9d1636d44ae31534470b0d14fe4b9ba
1wes <okemwawes@gmail.com>
2022-12-24 12:03:41 +0300

    use hashRouter to enable refreshing client-side rendered instructions route in github Pages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import ReactDom from 'react-dom';
 import React from 'react';
 import Game from './App';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {HashRouter, Routes, Route} from 'react-router-dom'
 import Howtoplay from './components/Instructions/how-to-play';
 
 const root=document.getElementById("root");
 
 ReactDom.render(
-  // all rendering goes inside BrowserRouter 
-  <BrowserRouter>
+  // all rendering goes inside HashRouter
+  // enables storing the URL in the hash portion since it cannot be sent to the server in github Pages
+
+  <HashRouter hashType='slash' basename='/' >
     <Routes>
-      <Route path='/' element={<Game />} />
-        <Route path='instructions' element={<Howtoplay />} />
-       <Route />
+      <Route path='/' element={<Game/>} />
+      <Route path='instructions' element={<Howtoplay/>} />
     </Routes>
-  </BrowserRouter>, root
+  </HashRouter>, root
 );
